03fecf69d8f5f2aad7e3d97568c931c7eef363c5
Roman Shestakov <neoroma@gmail.com>
2018-02-05 19:17:01 +0200

    Change the root div id for greater good

--- packages/the-app/src/index.js
+++ packages/the-app/src/index.js
@@ -4,15 +4,12 @@ import { Wink } from 'the-lib'
 
 const wink = new Wink(200, 500)
 
-const App = () => {
-  return (
-    <div>
-      <h2>This is my app</h2>
-      <p>going to sum up some numbers {wink.theSum()}</p>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h2>This is my app</h2>
+    <p>going to sum up some numbers {wink.theSum()}</p>
+  </div>
+)
 
-console.log(process.env)
-ReactDOM.render(<App/>, document.getElementById('bora-app'))
+ReactDOM.render(<App />, document.getElementById('the-app'))
 
