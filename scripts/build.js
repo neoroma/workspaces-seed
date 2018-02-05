1a51dd5137f222361226cf856057d058a2ca0450
Roman Shestakov <neoroma@gmail.com>
2018-02-05 18:51:58 +0200

    Add some postinstall scripts

---
+++ scripts/build.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk')
+const path = require('path')
+const fs = require('fs')
+
+const PACKAGES_DIR = path.resolve(__dirname, '../packages')
+const folders = fs.readdirSync(PACKAGES_DIR)
+const welcome = chalk.reset.inverse.bold.blue('Packages found in this repo : ')
+
+process.stdout.write(`${welcome}\n`)
+
+folders.forEach(f => {
+  const name = chalk.reset.inverse.bold.green(`   ${f}   `)
+  process.stdout.write(`${name}\n`)
+})
